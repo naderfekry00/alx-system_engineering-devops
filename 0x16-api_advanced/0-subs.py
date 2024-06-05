0aecf0bb4fd4d0794cbc842480e6a6901ad3ba9c
nader <naderfekry3@gmail.com>
2024-06-04 17:26:14 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {"User-agent": "Google Chrome Version 81.0.4044.129"}
+    user_agent = {"User-agent": "Google Chrome Version 125.0.6422.114"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
